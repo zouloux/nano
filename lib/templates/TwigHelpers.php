5a5c1f3332f3e5646326fa119abca9f0b7850496
Alexis Bouhet <alexis@les-hallucines.studio>
2024-07-19 16:21:57 +0200

    Added image twig helpers

--- lib/templates/TwigHelpers.php
+++ lib/templates/TwigHelpers.php
@@ -183,5 +183,41 @@ class TwigHelpers
 				return BlurHashHelper::blurHashToBase64PNGCached( $blurHashArray, $punch, $disableCache );
 			})
 		);
+
+		// --------------------------------------------------------------------- WP IMAGES
+		function browseCompatibleFormats ( $formats, $filter = null ) {
+			$supportsWebP = str_contains($_SERVER[ 'HTTP_ACCEPT' ], 'image/webp');
+			$r = [];
+			foreach ( $formats as $format ) {
+				if ( !$supportsWebP && $format['format'] === "webp" ) continue;
+				if ( $supportsWebP && $format['format'] !== "webp" ) continue;
+				$r[] = is_null($filter) ? $format : $filter( $format );
+			}
+			return $r;
+		}
+		$twig->addFilter(
+			new TwigFilter('imageSrcSet', function ( $image ) {
+				$image = is_array($image) ? $image : $image->toArray();
+				$sizes = browseCompatibleFormats($image['formats'], function ($format) {
+					return parse_url( $format['href'], PHP_URL_PATH )." ".$format['width']."w";
+				});
+				return implode(",", $sizes);
+			})
+		);
+		$twig->addFilter(
+			new TwigFilter('imageSrc', function ( $image, string|int $size = null ) {
+				$image = is_array($image) ? $image : $image->toArray();
+				$formats = browseCompatibleFormats($image['formats']);
+				$nearestFormat = null;
+				foreach ( $formats as $format ) {
+					if (
+						$nearestFormat === null ||
+						abs( $format['width'] - $size ) < abs( $nearestFormat['width'] - $size )
+					)
+						$nearestFormat = $format;
+				}
+				return parse_url($nearestFormat['href'], PHP_URL_PATH);
+			})
+		);
 	}
 }
